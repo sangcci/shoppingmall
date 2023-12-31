f27177400e301c1d93f2f287113f6feae956a716
baksakcci <pshsh910@gmail.com>
2023-12-31 20:04:10 +0900

    init: init project

---
+++ src/test/java/baksakcci/shoppingmall/ShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package baksakcci.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
