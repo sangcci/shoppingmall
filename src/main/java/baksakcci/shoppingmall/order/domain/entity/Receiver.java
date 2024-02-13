27aa5fd8a683ba82635255b5175a5779804841e7
baksakcci <pshsh910@gmail.com>
2024-02-13 16:30:53 +0900

    [#1]feat(Address, Receiver): 값 타입 Receiver, Address 불변성 보장 기능 추가
    - 속성 final 적용
    - equals(), hashCode() 적용

--- src/main/java/baksakcci/shoppingmall/order/domain/entity/Receiver.java
+++ src/main/java/baksakcci/shoppingmall/order/domain/entity/Receiver.java
@@ -1,12 +1,33 @@
 package baksakcci.shoppingmall.order.domain.entity;
 
+import java.util.Objects;
+
 public class Receiver {
-    private String receiverName;
-    private String receiverPhoneNumber;
+    private final String receiverName;
+    private final String receiverPhoneNumber;
 
     // constructor
     public Receiver(String receiverName, String receiverPhoneNumber) {
         this.receiverName = receiverName;
         this.receiverPhoneNumber = receiverPhoneNumber;
+    }
+
+    // immutable
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Receiver receiver = (Receiver) o;
+        return Objects.equals(receiverName, receiver.receiverName) && Objects.equals(
+                receiverPhoneNumber, receiver.receiverPhoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(receiverName, receiverPhoneNumber);
     }
 }
