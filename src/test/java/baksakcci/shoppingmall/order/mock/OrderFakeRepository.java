2e460bd33a3fc7ad16d2361fb146a53fafe514b7
baksakcci <pshsh910@gmail.com>
2024-05-27 15:56:15 +0900

    [#1]refactor(OrderFakeRepository): 주문 생성 로직 변경
    - Test를 위한 Fake Repository에서도 return 값이 없도록 수정

---
+++ src/test/java/baksakcci/shoppingmall/order/mock/OrderFakeRepository.java
@@ -0,0 +1,32 @@
+package baksakcci.shoppingmall.order.mock;
+
+import baksakcci.shoppingmall.order.domain.Order;
+import baksakcci.shoppingmall.order.application.port.OrderRepository;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+public class OrderFakeRepository implements OrderRepository {
+
+    private Long id = 1L;
+    private static final Map<Long, Order> orderStorage = new HashMap<>();
+
+    @Override
+    public void create(Order order) {
+        orderStorage.put(id++, order);
+    }
+
+    @Override
+    public Order findById(long id) {
+        return orderStorage.get(id);
+    }
+
+    @Override
+    public Order update(Order order) {
+        if (orderStorage.getOrDefault(order.getId(), null) == null) {
+            throw new NoSuchElementException();
+        }
+        orderStorage.put(order.getId(), order);
+        return orderStorage.get(order.getId());
+    }
+}
